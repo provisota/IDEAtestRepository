4cbc60a55b4febf1f2dfd1798e87ac54e06cf35f
Illiya Alterovych <provisota@ukr.net>
2013-12-15 01:41:46 +0200

    first commit
    Signed-off-by: Illiya Alterovych <provisota@ukr.net>

---
+++ threeThreads.java
@@ -0,0 +1,69 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class threeThreads
+{
+    public static byte countThreads = 3;
+    static List<Thread> threads = new ArrayList<Thread>(countThreads);
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        initThreadsAndStart();
+        Thread.sleep(3000);
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod()
+    {
+        for (int i = 0; i < countThreads; i++)
+        {
+            threads.get(i).interrupt();
+        }
+    }
+
+    private static void initThreadsAndStart()
+    {
+        Water water = new Water("water");
+        for (int i = 0; i < countThreads; i++)
+        {
+            threads.add(new Thread(water, "#" + i));
+        }
+
+        for (int i = 0; i < countThreads; i++)
+        {
+            threads.get(i).start();
+        }
+    }
+
+    public static class Water implements Runnable
+    {
+        private String commonResource;
+
+        public Water(String commonResource)
+        {
+            this.commonResource = commonResource;
+        }
+
+        public void run()
+        {
+            //fix 2 variables - исправь 2 переменных
+            boolean isCurrentThreadInterrupted = Thread.currentThread().isInterrupted();
+            String threadName = Thread.currentThread().getName();
+
+            try
+            {
+                while (!isCurrentThreadInterrupted)
+                {
+                    System.out.println("Объект " + commonResource + ", нить " + threadName);
+                    Thread.sleep(1000);
+                }
+            }
+            catch (InterruptedException e)
+            {
+            }
+        }
+    }
+
+}
