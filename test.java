1d6ac977e1fc11c09496b21b28422c6edd0b9683
Illiya Alterovych <provisota@ukr.net>
2013-12-25 18:32:55 +0200

    Second commit of test folder

--- test.java
+++ test.java
@@ -4,22 +4,49 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class test
+public class Test
 {
-    public static void main(String[] args) throws IOException
+    int x;
+    static int y;
+    public static void main(String[] args) throws IOException, CloneNotSupportedException
     {
-        newClass newClass1 = new newClass();
-        newClass1.x++;
 
-        newClass newClass2 = newClass1;
-        newClass2.x++;
-
-        System.out.println(newClass1.x);
-        System.out.println(newClass2.x);
+        x = 10;
+        y = 15;
+        B b = new B();
+        System.out.println(b.zero);
     }
 
-    private static class newClass
+    private static class Zero
     {
-        int x;
+        static int zero;
+        static
+        {
+            System.out.println(zero);
+        }
+        private Zero()
+        {
+            System.out.println("Class Zero Constructor");
+            zero = 5;
+            System.out.println(zero);
+        }
+    }
+
+    private static class A extends Zero
+    {
+        private A()
+        {
+            System.out.println("Class A Constructor");
+            System.out.println(zero);
+            zero = 10;
+        }
+    }
+
+    public static class B extends A
+    {
+        private B()
+        {
+            System.out.println("Class B Constructor");
+        }
     }
 }
